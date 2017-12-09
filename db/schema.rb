0fe6eed0ab88de88ffe7865eaa980fd76e6ff752
Paulo Moura <paulociecomp@gmail.com>
2017-12-09 17:05:14 -0300

    add postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171208190939) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "batches", force: :cascade do |t|
     t.string "description"
     t.string "product"
